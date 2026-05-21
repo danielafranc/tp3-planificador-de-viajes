ebde2ef59067c89990d031fc020f79ce501f133b
unknown <agustinakraemer@gmail.com>
2026-05-20 21:41:05 -0300

    Arreglo de Fecha/mes. Funcionando

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'core/router/app_router.dart';
 import 'core/theme/app_colors.dart';
@@ -32,6 +33,12 @@ class MainApp extends StatelessWidget {
           surface: AppColors.pearl,
         ),
       ),
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [Locale('es')],
     );
   }
 }
